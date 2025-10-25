5e4de66fb575a06dc4b43adf2d16f3e1d9c11501
gustavo <g.mercante@hotmail.com>
2025-10-25 17:45:00 -0300

    Reinstate simple Whitenoise storage and verify middleware order

--- valuation/settings.py
+++ valuation/settings.py
@@ -167,6 +167,7 @@ CELERY_TIMEZONE = 'America/Sao_Paulo' # Use seu timezone
 
 # E adicione esta linha para facilitar o gerenciamento
 STATIC_ROOT = BASE_DIR / 'staticfiles_build'
+WHITENOISE_ROOT = STATIC_ROOT
 #WHITENOISE_STATIC_PREFIX = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
