71af75a5fc8e7bf47a99d561ab636e14c420d71b
gustavo <g.mercante@hotmail.com>
2025-10-25 16:27:57 -0300

    Revert wsgi.py wrap, rely solely on WhiteNoise middleware

--- valuation/wsgi.py
+++ valuation/wsgi.py
@@ -1,27 +1,14 @@
 """
 WSGI config for valuation project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
-# --- ADICIONE ESTA IMPORTAÇÃO ---
-from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'valuation.settings') # Verifique se 'valuation.settings' está correto
+# Certifique-se que o nome 'valuation.settings' está correto
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'valuation.settings')
 
-# Pega a aplicação Django padrão
 application = get_wsgi_application()
-
-# --- ENVOLVE A APLICAÇÃO COM WHITENOISE ---
-# Isto permite ao Whitenoise servir ficheiros diretamente do STATIC_ROOT
-# Se STATICFILES_STORAGE foi removido/comentado em settings.py
-application = WhiteNoise(application)
-# Se você quiser manter STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage',
-# pode usar: application = WhiteNoise(application, root=settings.STATIC_ROOT)
-# Mas vamos tentar sem o STATICFILES_STORAGE primeiro.
+# Linha removida: application = WhiteNoise(application)
