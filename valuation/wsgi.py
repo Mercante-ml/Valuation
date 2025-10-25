1e622a863a77f93c20e88fb86b142648a486ef82
gustavo <g.mercante@hotmail.com>
2025-10-25 18:39:11 -0300

    Final configuration attempt for static files

--- valuation/wsgi.py
+++ valuation/wsgi.py
@@ -1,22 +1,9 @@
-"""
-WSGI config for valuation project.
-It exposes the WSGI callable as a module-level variable named ``application``.
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
+# valuation/wsgi.py
 import os
 from django.core.wsgi import get_wsgi_application
-# --- ADICIONE ESTAS IMPORTAÇÕES ---
-from whitenoise import WhiteNoise
-from django.conf import settings # Para aceder a STATIC_ROOT
 
 # Certifique-se que o nome 'valuation.settings' está correto
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'valuation.settings')
 
-# Pega a aplicação Django padrão
 application = get_wsgi_application()
-
-# --- ENVOLVE A APLICAÇÃO COM WHITENOISE, ESPECIFICANDO O ROOT ---
-# Isto diz explicitamente ao Whitenoise onde procurar os ficheiros
-# que foram coletados pelo collectstatic.
-#application = WhiteNoise(_application, root=settings.STATIC_ROOT)
+# NENHUMA linha do WhiteNoise aqui
